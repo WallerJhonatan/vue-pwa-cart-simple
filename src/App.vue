d3d8164481ff85591011f9ed6d353afa38361dbc
Waller Jhonatan <waller.jhonatanc@gmail.com>
2019-12-19 15:15:19 -0200

    Create views of product, cart and checkout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <BlockUI v-if="display" style="width: 30px">
+      <div align="center">
+        <img src="@/assets/logo.png" alt="" width="60">
+        <div class="loader"></div>
+      </div>
+    </BlockUI>
+    <theme>
+      <router-view/>
+    </theme>
   </div>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        display: false,
+      }
+    },
+    created () {
+      let self = this
+      this.$events.on('DisplayBlockUI', eventData => self.displayBlockUI(eventData))
+    },
+    methods: {
+      displayBlockUI (evt) {
+        this.display = evt
+      }
+    }
+  }
+
+</script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav {
-  padding: 30px;
-}
+  #nav {
+    padding: 30px;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
 </style>
